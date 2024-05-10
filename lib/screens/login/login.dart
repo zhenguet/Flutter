29d19d27a5d455c3e74770717d1684743fc697dc
Zuet <zuet@eup.net.vn>
2024-05-10 11:25:22 +0700

    update login UI

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatefulWidget {
 
 class LoginFormState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
+  bool passwordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +21,7 @@ class LoginFormState extends State<LoginScreen> {
       key: _formKey,
       child: Center(
         child: Container(
-          padding: const EdgeInsets.all(80.0),
+          padding: const EdgeInsets.all(40.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -28,46 +29,97 @@ class LoginFormState extends State<LoginScreen> {
                 'Welcome',
                 style: Theme.of(context).textTheme.displayLarge,
               ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Username',
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Password',
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter username',
+                    labelText: 'Username*',
+                    suffixIcon: const Icon(Icons.person),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Username cannot be empty';
+                    }
+                    return null;
+                  },
                 ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
               ),
-              const SizedBox(
-                height: 24,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Login success!')),
-                    );
-                    context.pushReplacement('/main');
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow,
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
                 ),
-                child: const Text('Login'),
-              )
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter password',
+                    labelText: 'Password*',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          passwordVisible = !passwordVisible;
+                        });
+                      },
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  obscureText: !passwordVisible,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password cannot be empty';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 12,
+                ),
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Login success!')),
+                      );
+                      context.pushReplacement('/main');
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[200],
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 32),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const Text("I forgot my password")
             ],
           ),
         ),
