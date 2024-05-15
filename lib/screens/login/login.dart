627dacf9560470fbc913fab77663c6fc45e7db5e
Zuet <zuet@eup.net.vn>
2024-05-15 15:49:38 +0700

    Update UI

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -26,134 +26,139 @@ class LoginFormState extends State<LoginScreen> {
         body: Form(
       key: _formKey,
       child: Center(
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                ),
-                child: const Stack(
-                  alignment: Alignment(0, 0.6),
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/zutter.png'),
-                      radius: 100,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                ),
-                child: RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.displayLarge,
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: AppLocalizations.of(context)!.helloWorld,
-                      ),
-                      const TextSpan(
-                        text: 'Zutter',
-                        style: TextStyle(
-                          color: Colors.blue, // Đổi màu của từ "Zutter"
-                        ),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 450,
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                  ),
+                  child: const Stack(
+                    alignment: Alignment(0, 0.6),
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/zutter.png'),
+                        radius: 100,
                       ),
                     ],
                   ),
                 ),
-              ),
-              TextForm(
-                hintText: 'Enter username',
-                labelText: 'Username*',
-                suffixIcon: const Icon(Icons.person),
-                onChanged: (value) {
-                  setState(() {
-                    username = value;
-                  });
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Username cannot be empty';
-                  }
-                  return null;
-                },
-              ),
-              TextForm(
-                hintText: 'Enter password',
-                labelText: 'Password*',
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    passwordVisible ? Icons.visibility : Icons.visibility_off,
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
                   ),
-                  onPressed: () {
+                  child: RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.displayLarge,
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: AppLocalizations.of(context)!.helloWorld,
+                        ),
+                        const TextSpan(
+                          text: 'Zutter',
+                          style: TextStyle(
+                            color: Colors.blue, // Đổi màu của từ "Zutter"
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                TextForm(
+                  hintText: 'Enter username',
+                  labelText: 'Username*',
+                  suffixIcon: const Icon(Icons.person),
+                  onChanged: (value) {
                     setState(() {
-                      passwordVisible = !passwordVisible;
+                      username = value;
                     });
                   },
-                ),
-                obscureText: !passwordVisible,
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password cannot be empty';
-                  }
-                  return null;
-                },
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 12,
-                ),
-                width: double.infinity,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Login success!')),
-                      );
-                      context.pushReplacement('/main');
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Username cannot be empty';
                     }
+                    return null;
                   },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue[200],
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16, horizontal: 32),
+                ),
+                TextForm(
+                  hintText: 'Enter password',
+                  labelText: 'Password*',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        passwordVisible = !passwordVisible;
+                      });
+                    },
                   ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                  obscureText: !passwordVisible,
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password cannot be empty';
+                    }
+                    return null;
+                  },
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 12,
+                  ),
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Login success!')),
+                        );
+                        context.pushReplacement('/main');
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[200],
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 32),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              InkWell(
-                onTap: () {
-                  log("I forgot my password clicked");
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "I forgot my password",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 18,
+                InkWell(
+                  onTap: () {
+                    log("I forgot my password clicked");
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "I forgot my password",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,25 +1,116 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class Settings extends StatefulWidget {
-  const Settings({Key? key}) : super(key: key);
+  const Settings({super.key});
 
   @override
   _SettingsState createState() => _SettingsState();
 }
 
 class _SettingsState extends State<Settings> {
+  final List<Map<String, String>> items = [
+    {'id': 'en', 'value': 'Tiếng Anh'},
+    {'id': 'vi', 'value': 'Tiếng Việt'},
+  ];
+  late String? item = items.first['value'];
+  bool _isDarkModeEnabled = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ElevatedButton(
-        onPressed: () {
-          context.pushReplacement('/login');
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.yellow,
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(children: [
+                Row(
+                  children: [
+                    const Text('Chọn ngôn ngữ: '),
+                    DropdownButton2<String>(
+                      value: item,
+                      onChanged: (String? newValue) {
+                        if (newValue != null) {
+                          setState(() {
+                            item = newValue;
+                          });
+                        }
+                      },
+                      items: items.map((Map<String, String> item) {
+                        return DropdownMenuItem<String>(
+                          value: item['value'],
+                          child: Text(item['value']!),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text("Chọn chủ đề: "),
+                    const Icon(Icons.wb_sunny),
+                    Switch(
+                      value: _isDarkModeEnabled,
+                      onChanged: (bool newValue) {
+                        setState(() {
+                          _isDarkModeEnabled = newValue;
+                        });
+                      },
+                    ),
+                    const Icon(Icons.nightlight_round),
+                  ],
+                ),
+              ]),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Logout'),
+                      content: const Text('Are you sure you want to logout?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            context.pushReplacement('/login');
+                          },
+                          child: const Text('Logout'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                ),
+                child: const Text(
+                  'Logout',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        child: const Text('Logout'),
       ),
     );
   }
